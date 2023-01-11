41b665a51ff62005a1711f87e2259a41cbdcdd03
pgdonato <donatogabriel@outlook.com>
2023-01-10 23:14:37 -0300

    Update

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,21 +1,20 @@
-document.getElementById("tickets").onclick =  function(){
-    location.href="https://pgdonato.github.io/IntegradorFrontEnd-CAC/tickets.html";
-};
 
 const valorTicket = 200;
-function calcTotal() {
-    let cant=document.getElementById("cant").value
-    
-    let categoria=document.getElementById("categoria").value
+let total=0
 
-    let total=0
-    if (categoria==1) {
+function calcTotal() {
+    console.log("total");
+    let total=document.getElementById("total").value
+    let cant=document.getElementById("cantidad").value
+    let cat=document.getElementById("categoria").value
+
+    if (cat==1) {
         total=cant*valorTicket*.2 //80%
-    } else if (categoria==2) {
+        console.log(total);
+    } else if (cat==2) {
         total=cant*valorTicket*.5 //50%
     } else {
         total=cant*valorTicket*.85 //15%
     }
-
     document.getElementById("total").innerHTML=`Total a pagar: $${total}`
 };
